f0086b842e4598ed24cad54b360592751c7e3c11
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-28 23:05:28 +0200

    Added IWarehouse Interface

--- Warehouse/Warehouse/IWarehouse.cs
+++ Warehouse/Warehouse/IWarehouse.cs
@@ -2,7 +2,11 @@
 
 namespace Warehouse
 {
-    public class Class1
+    public interface IWarehouse
     {
+        bool HasProduct(string product);
+        int CurrentStock(string product);
+        void AddStock(string product, int amount);
+        void TakeStock(string product, int amount);
     }
 }
