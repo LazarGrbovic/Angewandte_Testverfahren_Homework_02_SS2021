97a5987a9818f316d77364118e91c56df8f622bc
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-29 01:07:00 +0200

    Added Tests of the Warehouse Functionality

--- Warehouse/Warehouse.Test/Warehouse.Tests.cs
+++ Warehouse/Warehouse.Test/Warehouse.Tests.cs
@@ -75,4 +75,84 @@ namespace Warehouse.Test
         }
         #endregion
     }
+
+    [TestClass]
+    public class Warehouse_Functionality_Tests
+    {
+        #region AddStock_Method
+        public void Warehouse_AddStock_Method_Test(string productName, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock("SomeProduct1", 1);
+            w.AddStock("SomeProduct2", 2);
+            w.AddStock("SomeProduct3", 3);            
+            w.AddStock(productName, amount);
+            Assert.AreEqual(true, w.HasProduct(productName)); 
+        }        
+        #endregion
+
+        #region CurrentStock_Method
+        [DataTestMethod]
+        [DataRow("TestProduct1", 1)]
+        [DataRow("TestProduct2", 2)]
+        [DataRow("TestProduct3", 3)]
+        public void Warehouse_CurrentStock_Method_Test(string productName, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock("SomeProduct1", 1);
+            w.AddStock("SomeProduct2", 2);
+            w.AddStock("SomeProduct3", 3);
+            w.AddStock(productName, amount);
+            Assert.AreEqual(amount, w.CurrentStock(productName));
+        }
+        #endregion CurrentStock_Method
+
+        #region HasStock_Method
+        [DataTestMethod]
+        [DataRow("TestProduct1", 1)]
+        [DataRow("TestProduct2", 2)]
+        [DataRow("TestProduct3", 3)]
+        public void Warehouse_HasStock_Method_Test(string productName, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock("SomeProduct1", 1);
+            w.AddStock("SomeProduct2", 2);
+            w.AddStock("SomeProduct3", 3);
+            w.AddStock(productName, amount);
+            Assert.AreEqual(true, w.HasProduct(productName));
+        }
+        #endregion HasStock_Method
+
+        #region TakeStock_Method
+        [DataTestMethod]
+        [DataRow("TestProduct1", 10, 1)]
+        [DataRow("TestProduct2", 20, 10)]
+        [DataRow("TestProduct3", 30, 20)]
+        public void Warehouse_TakeStock_Method_Takes_Successfully_Given_Amount_Test(string productName, int amount, int amountToTake)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock("SomeProduct1", 1);
+            w.AddStock("SomeProduct2", 2);
+            w.AddStock("SomeProduct3", 3);
+            w.AddStock(productName, amount);
+            w.TakeStock(productName, amountToTake);
+            Assert.AreEqual(amount - amountToTake, w.CurrentStock(productName));
+        }
+
+        [DataTestMethod]
+        [DataRow("TestProduct1", 10)]
+        [DataRow("TestProduct2", 20)]
+        [DataRow("TestProduct3", 30)]
+        public void Warehouse_TakeStock_Method_Removes_Product_When_It_Is_Sold_Out_Test(string productName, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock("SomeProduct1", 1);
+            w.AddStock("SomeProduct2", 2);
+            w.AddStock("SomeProduct3", 3);
+            w.AddStock(productName, amount);
+            w.TakeStock(productName, amount);
+            Assert.AreEqual(false, w.HasProduct(productName));            
+        }
+        #endregion TakeStock_Method
+    }
 }
