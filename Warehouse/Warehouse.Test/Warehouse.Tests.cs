8c74f2e637cdd23176a5b998a97492814d5d0b83
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-28 23:31:41 +0200

    Added Exceptions Test of the InvalidProductAmountException

--- Warehouse/Warehouse.Test/Warehouse.Tests.cs
+++ Warehouse/Warehouse.Test/Warehouse.Tests.cs
@@ -46,5 +46,33 @@ namespace Warehouse.Test
             w.TakeStock(name, amount);
         }
         #endregion
+        
+        #region InvalidProductAmountException
+        [DataTestMethod]
+        [DataRow("Product", 0)]
+        [DataRow("Product", -1)]
+        [DataRow("Product", -10)]
+        [DataRow("Product", -100)]
+        [DataRow("Product", -1000)]
+        [ExpectedException(typeof(InvalidProductAmountException))]
+        public void Warehouse_Add_Method_Throws_Exception_On_Invalid_Amount(string name, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.AddStock(name, amount);
+        }
+            
+        [DataTestMethod]
+        [DataRow("Product", 0)]
+        [DataRow("Product", -1)]
+        [DataRow("Product", -10)]
+        [DataRow("Product", -100)]
+        [DataRow("Product", -1000)]
+        [ExpectedException(typeof(InvalidProductAmountException))]
+        public void Warehouse_TakeStock_Method_Throws_Exception_On_Invalid_Amount(string name, int amount)
+        {
+            var w = new WarehouseImplementation();
+            w.TakeStock(name, amount);
+        }
+        #endregion
     }
 }
