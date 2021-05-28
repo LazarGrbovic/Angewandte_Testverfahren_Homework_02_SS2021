ee106654590efdac10d7bb68b456f7edf2362958
LazarGrbovic <lazarmgrbovic@gmail.com>
2021-05-28 23:06:57 +0200

    Created Warehouse.Implementation Class

---
+++ Warehouse/Warehouse/Warehouse.Implementation.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Warehouse
+{
+    public class WarehouseImplementation : IWarehouse
+    {
+        public void AddStock(string product, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CurrentStock(string product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasProduct(string product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeStock(string product, int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
